167c2d51b9928be721adf45c1663fd40a123a33c
Krishna <Krishna@LAPTOP-SQPGO4DR>
2017-02-25 23:34:50 +0530

    Refactored a bit

--- src/com/gola/practice/collections/UnboundedStack.java
+++ src/com/gola/practice/collections/UnboundedStack.java
@@ -6,12 +6,12 @@ import java.util.Random;
 
 public class UnboundedStack<E> implements Stack<E> {
 
-    @SuppressWarnings("hiding")
+    @SuppressWarnings ( "hiding" )
     private class Node<E> {
         E       ele;
         Node<E> next;
 
-        public Node(E ele) {
+        public Node ( E ele ) {
             this.ele = ele;
             this.next = null;
         }
@@ -22,7 +22,7 @@ public class UnboundedStack<E> implements Stack<E> {
     private int     size;
 
     @Override
-    public int size() {
+    public int size () {
         return size;
     }
 
@@ -34,22 +34,22 @@ public class UnboundedStack<E> implements Stack<E> {
     }
 
     @Override
-    public boolean isEmpty() {
+    public boolean isEmpty () {
         return size <= 0;
     }
 
     @Override
-    public boolean contains(Object o) {
+    public boolean contains ( Object o ) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Iterator<E> iterator() {
+    public Iterator<E> iterator () {
         return new LinkedStackIterator<>(top);
     }
 
     @Override
-    public boolean push(E e) {
+    public boolean push ( E e ) {
         Node<E> newTop = new Node<E>(e);
         newTop.next = top;
         top = newTop;
@@ -58,7 +58,7 @@ public class UnboundedStack<E> implements Stack<E> {
     }
 
     @Override
-    public E pop() {
+    public E pop () {
         Node<E> temp = top;
         top = top.next;
         --size;
@@ -66,26 +66,26 @@ public class UnboundedStack<E> implements Stack<E> {
     }
 
     @Override
-    public void clear() {
+    public void clear () {
         top = null; // Let the GC take care of garbage
         size = 0;
     }
 
-    @SuppressWarnings("hiding")
+    @SuppressWarnings ( "hiding" )
     private class LinkedStackIterator<E> implements Iterator<E> {
         private Node<E> _top;
 
-        public LinkedStackIterator(Node<E> top) {
+        public LinkedStackIterator ( Node<E> top ) {
             this._top = top;
         }
 
         @Override
-        public boolean hasNext() {
+        public boolean hasNext () {
             return _top != null;
         }
 
         @Override
-        public E next() {
+        public E next () {
             if (!hasNext()) {
                 throw new NoSuchElementException();
             }
@@ -96,51 +96,13 @@ public class UnboundedStack<E> implements Stack<E> {
     }
 
     @Override
-    public String toString() {
-	StringBuilder sb = new StringBuilder();
-	sb.append("[ ");
-	for (E e : this) {
-	    sb.append(e).append(" ");
-	}
-	sb.append("]");
-	return sb.toString();
-    }
-
-    public static void main(String[] args) {
-        Stack<Integer> intStack = new UnboundedStack<>();
-        Random random = new Random();
-
-        for (int i = 0; i < 15; i++) {
-            intStack.push(random.nextInt(100));
+    public String toString () {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ ");
+        for (E e : this) {
+            sb.append(e).append(" ");
         }
-        System.out.println("Stack Size: " + intStack.size());
-        System.out.println(intStack);
-
-        System.out.println("Popped Item: " + intStack.pop());
-        System.out.println("Popped Item: " + intStack.pop());
-        System.out.println("Popped Item: " + intStack.pop());
-        System.out.println("Popped Item: " + intStack.pop());
-        System.out.println("Popped Item: " + intStack.pop());
-
-        System.out.println("Stack Size: " + intStack.size());
-        System.out.println(intStack);
-
-        System.out.println("Popped Item: " + intStack.pop());
-        System.out.println("Popped Item: " + intStack.pop());
-        System.out.println("Popped Item: " + intStack.pop());
-        System.out.println("Popped Item: " + intStack.pop());
-        System.out.println("Popped Item: " + intStack.pop());
-
-        System.out.println("Stack Size: " + intStack.size());
-        System.out.println(intStack);
-
-        for (int i = 0 ; i < 10 ; i++ ) {
-            intStack.push(random.nextInt(200));
-        }
-
-        System.out.println("Stack Size: " + intStack.size());
-        System.out.println(intStack);
-
-        intStack.push(100); // Should give RuntimeException because of Stack Overflow
+        sb.append("]");
+        return sb.toString();
     }
 }
